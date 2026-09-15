6b42f27758a47bc4536a46e91ba9cfa448c912a7
dylanlionel959-del <dylanlionel959@gmail.com>
2026-09-15 07:34:21 -0400

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
   // Lista de códigos ISO de países que deseas bloquear (ej: 'US' para Estados Unidos, 'CN' para China)
-  const paisesBloqueados = ['US', 'CN', 'RU'];
+  const paisesBloqueados = ['VE', 'CN', 'RU'];
 
   // Cloudflare detecta automáticamente el país del visitante
   const codigoPais = context.request.cf ? context.request.cf.country : null;
